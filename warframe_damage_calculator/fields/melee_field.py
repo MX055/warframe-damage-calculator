e78cf12d699b6123faefa5d459dbd0d4d2aa2d40
MX.55 <83025703+AAAA0001@users.noreply.github.com>
2026-07-04 11:02:00 +0200

    Delete warframe_damage_calculator directory

--- warframe_damage_calculator/fields/melee_field.py
+++
@@ -1,5 +0,0 @@
-from .weapon import WeaponField
-
-
-class MeleeField(WeaponField):
-    attack_speed: float
--- warframe_damage_calculator/states/secondary.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from .ranged import RangedState
-
-
-@dataclass
-class SecondaryState(RangedState):
-    secondary_enervate: int = 0
-    secondary_encumber: float = 0.0
