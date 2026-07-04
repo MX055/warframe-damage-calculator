33cc99eda726601aa11c29fe804432f832de5024
MX.55 <83025703+AAAA0001@users.noreply.github.com>
2026-07-04 13:26:21 +0200

    Add files via upload

--- warframe_damage_calculator/models/dist.py
+++ warframe_damage_calculator/models/dist.py
@@ -3,58 +3,52 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Iterator, Iterable, Unpack
 from types import MappingProxyType
+from collections.abc import Mapping
 
 from ..utils import PHYSICAL_TYPES, ELEMENTAL_TYPES, DAMAGE_TYPES, ELEMENTAL_COMBINATIONS, DAMAGE_TYPE_ORDER, DamageType
 from ..fields import DamageField
 
 
-@dataclass(frozen=True, init=False)
+@dataclass(frozen=True, init=False, slots=True)
 class dist:
-    _dist: dict[DamageType, float] = field(default_factory=dict)
-    _total_damage: float = 0.0
+    dist: Mapping[DamageType, float]
+    total_damage: float
     
     def __init__(self, **kwargs: Unpack[DamageField]) -> None:
-        if any(dt not in DAMAGE_TYPES for dt in kwargs):
-            raise ValueError
         sorted_items = dict(sorted(kwargs.items(), key=lambda item: DAMAGE_TYPE_ORDER[item[0]]))
-        object.__setattr__(self, '_dist', sorted_items)
-        object.__setattr__(self, '_total_damage', sum(sorted_items.values()))
+        object.__setattr__(self, "dist", MappingProxyType(sorted_items))
+        object.__setattr__(self, 'total_damage', sum(sorted_items.values()))
 
     def __add__(self, other: dist) -> dist:
-        return dist(**{dt: self.get(dt) + other.get(dt) for dt in self.dist | other.dist})
+        if isinstance(other, dist):
+            return dist(**{dt: self.get(dt) + other.get(dt) for dt in self.dist | other.dist})
+        else: return NotImplemented
     
     def __radd__(self, other: int | float) -> dist:
         if other == 0: return self
         else: return NotImplemented
     
     def __mul__(self, other: int | float) -> dist:
-        return dist(**{dt: d * other for dt, d in self})
+        if isinstance(other, (int, float)):
+            return dist(**{dt: d * other for dt, d in self})
+        else: return NotImplemented
     
     def __rmul__(self, other: int | float) -> dist:
-        return dist(**{dt: other * d for dt, d in self})
+        if isinstance(other, (int, float)):
+            return dist(**{dt: other * d for dt, d in self})
+        else: return NotImplemented
     
     def __iter__(self) -> Iterator[tuple[DamageType, float]]:
         return iter(self.dist.items())
     
     def __hash__(self) -> int:
-        return hash(tuple(sorted(self.dist.items())))
+        return hash(tuple(self.dist.items()))
 
     def __str__(self) -> str:
         return ", ".join(f"{dt.upper()}: {d}" for dt, d in self)
     
     def __repr__(self) -> str:
         return f"dist({', '.join(f'{dt}={d}' for dt, d in self)})"
-    
-    @property
-    def dist(self) -> dict[DamageType, float]:
-        return MappingProxyType(self._dist)
-    
-    @property
-    def total_damage(self) -> float:
-        return self._total_damage
-    
-    def get(self, dt: DamageType) -> float:
-        return self.dist.get(dt, 0)
     
     def combine(self) -> dist:
         elements = list(self.include(ELEMENTAL_TYPES))
@@ -81,11 +75,18 @@ class dist:
     def positive(self) -> dist:
         return dist(**{dt: d for dt, d in self if d > 0})
     
-    def weight(self, dt: DamageType) -> float:
-        if not isinstance(dt, str):
-            raise TypeError
+    def get(self, dt: DamageType) -> float:
         if dt not in DAMAGE_TYPES:
             raise ValueError
+        if not isinstance(dt, str):
+            raise TypeError
+        return self.dist.get(dt, 0) 
+    
+    def weight(self, dt: DamageType) -> float:
+        if dt not in DAMAGE_TYPES:
+            raise ValueError
+        if not isinstance(dt, str):
+            raise TypeError
         return 0.0 if self.total_damage == 0 else self.get(dt) / self.total_damage
     
     def apply(self, other: dist) -> dist:
