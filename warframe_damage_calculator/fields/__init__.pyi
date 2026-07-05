79275541c52fafce3257a3668a5309d700987e87
MX.55 <83025703+AAAA0001@users.noreply.github.com>
2026-07-05 12:16:02 +0200

    Delete warframe_damage_calculator/fields/__init__.pyi

--- warframe_damage_calculator/fields/__init__.pyi
+++
@@ -1,8 +0,0 @@
-from .damage_field import DamageField as DamageField
-from .melee_field import MeleeField as MeleeField
-from .primary_field import PrimaryField as PrimaryField
-from .ranged_field import RangedField as RangedField
-from .secondary_field import SecondaryField as SecondaryField
-from .weapon_field import WeaponField as WeaponField
-
-__all__: list[str]
