7d1f66a17233dcf0a2d787b695c02ce6bfcbf674
Marcos Escobar <marcos.escobar.es@gmail.com>
2026-07-12 17:54:04 +0200

    fix

--- warframe_damage_calculator/__init__.py
+++ warframe_damage_calculator/__init__.py
@@ -1,9 +1,10 @@
-from .models import Upgrade, Build, Melee, Primary, Secondary
+from .models import dist, Upgrade, Build, Melee, Primary, Secondary
 from .data import arsenal
 
 __version__ = "0.5.0"
 
 __all__ = [
+    "dist",
     "Upgrade",
     "Build",
     "Melee",
