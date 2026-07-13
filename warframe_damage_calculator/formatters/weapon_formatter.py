b4bfee171d37b09d9316eac38d325a3a08e06349
Marcos Escobar <marcos.escobar.es@gmail.com>
2026-07-13 09:12:23 +0200

    fix

--- warframe_damage_calculator/formatters/weapon_formatter.py
+++ warframe_damage_calculator/formatters/weapon_formatter.py
@@ -12,7 +12,6 @@ class WeaponFormatter[TWeaponState: WeaponState]:
         raise NotImplementedError
     
     def upgrades(self) -> str:
-        
         contributions = self.calculator.contribution_proportions
         max_len = max(len(name) for name in contributions)
         return "\n".join(f"{f'{name}:':<{max_len + 1}} {contribution:.2%}" for name, contribution in contributions.items())
